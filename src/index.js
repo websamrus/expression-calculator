b1c12184a4d0207a90c8e426183741efb918ab3e
websamrus <websamrus@gmail.com>
2019-10-14 18:10:32 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let left = expr.split("").filter(el => el == "(");
+    let right = expr.split("").filter(el => el == ")");
+
+    if(left.length !== right.length) throw "ExpressionError: Brackets must be paired";
+    if (expr.includes("/ 0") || expr.includes("/0")) throw "TypeError: Division by zero.";
+    var r = new Function('return '+ expr );
+    var res = +r();
+
+    if (res == Infinity) throw "TypeError: Division by zero.";
+
+    return res;
 }
 
 module.exports = {
